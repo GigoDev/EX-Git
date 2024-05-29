c8461af0f3bd3afc46ff68a547b96cf5b0f32135
Gigo Dev <guyrick21@gmail.com>
2024-05-29 16:35:57 +0300

    Add dynemic ball diameter

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 'use strict'
 
-function onBallClick(){
-    console.log('hi')
+
+let gDiameter = 100
+
+
+function onBallClick() {
+    let elBall = document.querySelector('.ball')
+
+    gDiameter += 50
+    elBall.style.height = `${gDiameter}px`
+    elBall.style.width = `${gDiameter}px`
 }
